6823d4203b16e07bed7c70c08914c82f75d17173
telnorp <danielmaltchikov@protonmail.com>
2024-08-28 21:51:19 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,71 +9,27 @@ namespace binary
 
         private void updateOut()
         {
-            int sum = 0;
-
-            if (bool128.Checked)
-                sum += 128;
-            if (bool64.Checked)
-                sum += 64;
-            if (bool32.Checked)
-                sum += 32;
-            if (bool16.Checked)
-                sum += 16;
-            if (bool8.Checked)
-                sum += 8;
-            if (bool4.Checked)
-                sum += 4;
-            if (bool2.Checked)
-                sum += 2;
-            if (bool1.Checked)
-                sum += 1;
+            // Calculate the sum in a branchless manner using bitwise operations
+            int sum = (bool128.Checked ? 128 : 0) |
+                      (bool64.Checked ? 64 : 0) |
+                      (bool32.Checked ? 32 : 0) |
+                      (bool16.Checked ? 16 : 0) |
+                      (bool8.Checked ? 8 : 0) |
+                      (bool4.Checked ? 4 : 0) |
+                      (bool2.Checked ? 2 : 0) |
+                      (bool1.Checked ? 1 : 0);
 
             outputBits.Text = sum.ToString();
         }
 
-        private void bool128_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool64_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool32_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool16_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool8_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool4_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool2_CheckedChanged(object sender, EventArgs e)
-        {
-            updateOut();
-        }
-
-        private void bool1_CheckedChanged(object sender, EventArgs e)
+        private void bool_CheckedChanged(object sender, EventArgs e)
         {
             updateOut();
         }
 
         private void outputBits_TextChanged(object sender, EventArgs e)
         {
-         //TODO: make a stable in/out system.
+            //TODO: make a stable in/out system.
         }
     }
 }
